c17c07662e5e6cac696d9275712dae1fc4bbbe28
coopbell <cooper-bell@uiowa.edu>
2019-10-08 11:13:13 -0500

    Adding functionality to read from specified .m file not in directory and write it to a temp .m file that is in the directory

--- ngfmPlotSpectra.m
+++ ngfmPlotSpectra.m
@@ -2,6 +2,31 @@ subplot('position', [0.55 0.10 0.39 0.85]);
 M(spectra);
 run(M(spectra))
 
+
+%open file
+read_fid = fopen('/Users/cooperbell/Misc/external_script/PlotPSD1.m');
+
+% make temp folder
+tmp = tempname;
+mkdir(tmp);
+
+% Write MATLAB code to a file in the folder.
+newFile = fullfile(tmp,'ANewFile.m');
+write_fid = fopen(newFile,'w');
+
+% read file and write to temp file in our directory
+tline = fgets(read_fid);
+while ischar(tline)
+    fprintf(write_fid,tline);
+    tline = fgets(read_fid);
+end
+fclose(read_fid);
+fclose(write_fid);
+
+%Run the script.
+
+run(newFile)
+
 % if (strcmp(spectra,'psd'))
 %     PlotPSD;
 % else

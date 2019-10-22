772625af08cba5f3aeececfa06a7c84fe46e48f2
coopbell <cooper-bell@uiowa.edu>
2019-10-21 20:34:26 -0500

    fixed error checking for importing bad plots

--- ngfmPlotSpectra.m
+++ ngfmPlotSpectra.m
@@ -1,5 +1,7 @@
 global spectra;
 global plots;
+
+prev_spectra = spectra;
 % Spectra plot setup
 subplot('position', [0.55 0.10 0.39 0.85]);
 
@@ -10,5 +12,7 @@ catch exception
     % remove spectra from list
     index = find(strcmp(plots, spectra));
     plots(index) = [];
+    spectra = string(plots(1));
+    plotHandles.current_plot_menu.String = plots;
     warning(exception.identifier, 'Unable to load plot, %s', exception.message)
 end 
